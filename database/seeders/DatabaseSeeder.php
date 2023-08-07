2d707811f8e9f07a3033416e718c4e7a3466f5f3
Teddy <teddyrinah@gmail.com>
2023-08-07 19:41:42 +0200

    dashbord

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolePermissionSeeder::class,
             CompanySeeder::class,
-            CountriesTableSeeder::class
+            CountriesTableSeeder::class,
+            UserSeeder::class,
         ]);
 
     }
